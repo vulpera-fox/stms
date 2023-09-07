5671df8f9aaf37a0430ae69b2db7a644e3354a09
vulpera-fox <vulpera.yeowoo@gmail.com>
2023-09-07 16:45:26 +0900

    pjt detail 화면 및 기능구현

--- src/main/java/com/project/stms/service/project/ProjectMapper.java
+++ src/main/java/com/project/stms/service/project/ProjectMapper.java
@@ -3,10 +3,10 @@ package com.project.stms.service.project;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import com.project.stms.command.FileVO;
 import com.project.stms.command.ProjectVO;
+import com.project.stms.command.UserVO;
 
 @Mapper
 public interface ProjectMapper {
@@ -14,5 +14,9 @@ public interface ProjectMapper {
 	public List<ProjectVO> getList();
 	
 	public void insertFiles(List<FileVO> list);
+	
+	public ProjectVO getProjectDetail(int pjt_id);
+	
+	public List<UserVO> getUserDetail(int pjt_id);
 
 }
