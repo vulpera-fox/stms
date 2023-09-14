3c08559ee6a6a5d487a6955f8f1b717442d61493
ᙏ̤̫͚ yoonjeong ᙏ̤̫͚ <sasohameul@YoonJeongui-MacBookAir.local>
2023-09-14 20:10:27 +0900

    새소식 게시판 완료 및 Q&A 진행중(+폴더명 변경)

--- src/main/java/com/project/stms/service/user/UserService.java
+++ src/main/java/com/project/stms/service/user/UserService.java
@@ -1,17 +1,12 @@
-package com.project.stms.user.service;
+package com.project.stms.service.user;
 
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Mapper;
-
 import com.project.stms.command.UserVO;
 
-
-@Mapper	
-public interface UserMapper {	
+public interface UserService {
 	public int join(UserVO userVO);
-	public UserVO login(String user_email);
 	public int changePW(UserVO userVO);
 	public List<UserVO> userInfo(String user_email);
 	public UserVO checkId(String user_email);
