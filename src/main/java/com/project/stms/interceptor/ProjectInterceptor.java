a2a5ac76446653bb3552a05cae95cdd7edad7ae6
JH921029 <yjhzea@gmail.com>
2023-09-21 12:16:57 +0900

    로그인 타임아웃 시간

--- src/main/java/com/project/stms/interceptor/ProjectInterceptor.java
+++ src/main/java/com/project/stms/interceptor/ProjectInterceptor.java
@@ -1,43 +1,43 @@
-package com.project.stms.interceptor;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.project.stms.service.project.ProjectService;
-
-public class ProjectInterceptor implements HandlerInterceptor {
-	
-	
-
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-
-	}
-
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-		HttpSession session = request.getSession();
-
-		String user_email = (String) session.getAttribute("user_email");
-		
-		String user_role = (String) session.getAttribute("user_role");
-		
-
-		request.setAttribute("user_email", user_email);
-		
-		System.out.println(user_email + " 유저이메일");
-		
-		System.out.println(user_role + " 유저권한");
-		
-		return true;
-
-	}
-
-}
+//package com.project.stms.interceptor;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import com.project.stms.service.project.ProjectService;
+//
+//public class ProjectInterceptor implements HandlerInterceptor {
+//	
+//	
+//
+//	@Override
+//	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+//			ModelAndView modelAndView) throws Exception {
+//
+//	}
+//
+//	@Override
+//	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+//			throws Exception {
+//		HttpSession session = request.getSession();
+//
+//		String user_email = (String) session.getAttribute("user_email");
+//		
+//		String user_role = (String) session.getAttribute("user_role");
+//		
+//
+//		request.setAttribute("user_email", user_email);
+//		
+//		System.out.println(user_email + " 유저이메일");
+//		
+//		System.out.println(user_role + " 유저권한");
+//		
+//		return true;
+//
+//	}
+//
+//}
